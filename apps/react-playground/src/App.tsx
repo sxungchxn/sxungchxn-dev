3aa92d2e612dccd6067652babd0a7a5cc774175f
Yangseungchan <uvzone@naver.com>
2024-01-20 18:09:32 +0900

    react-playground 초기화

---
+++ apps/react-playground/src/App.tsx
@@ -0,0 +1,34 @@
+import { useRef, useState } from 'react'
+import './App.css'
+import { IconSun } from '@seungchan-dev/icons'
+import { Icon } from '@seungchan-dev/ui'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const ref = useRef<SVGSVGElement | null>(null)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          {/*<img src={viteLogo} className="logo" alt="Vite logo" />*/}
+        </a>
+        <a href="https://react.dev" target="_blank">
+          {/*<img src={reactLogo} className="logo react" alt="React logo" />*/}
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <IconSun ref={ref} />
+      <Icon source={IconSun} />
+    </>
+  )
+}
+
+export default App
