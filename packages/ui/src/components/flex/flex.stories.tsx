afd2cbe4abada1208c9793687af38d2fdf393f09
Yangseungchan <uvzone@naver.com>
2024-02-04 23:48:52 +0900

    feat: Flex 컴포넌트 추가

---
+++ packages/ui/src/components/flex/flex.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Flex, type FlexProps } from './flex'
+import { Text } from '@/components/text'
+import { layoutArgTypes } from '@/stories/argTypes'
+
+const meta: Meta<typeof Flex> = {
+  title: 'atoms/Flex',
+  tags: ['autodocs'],
+  component: Flex,
+  argTypes: {
+    ...layoutArgTypes,
+  },
+}
+
+export default meta
+
+type Story = StoryObj<FlexProps>
+
+const Template = (args: FlexProps) => {
+  return <Flex {...args}></Flex>
+}
+
+export const Default: Story = {
+  render: Template,
+  args: {
+    gap: '8px',
+    color: 'purple500',
+    backgroundColor: 'purple100',
+    children: (
+      <>
+        <Text variant={'nav2'}>item1</Text>
+        <Text variant={'nav2'}>item2</Text>
+        <Text variant={'nav2'}>item3</Text>
+        <Text variant={'nav2'}>item4</Text>
+        <Text variant={'nav2'}>item5</Text>
+        <Text variant={'nav2'}>item6</Text>
+      </>
+    ),
+  },
+}
+
+export const FlexColumn: Story = {
+  render: Template,
+  args: {
+    gap: '8px',
+    direction: 'column',
+    color: 'purple500',
+    backgroundColor: 'purple100',
+    children: (
+      <>
+        <Text variant={'nav2'}>item1</Text>
+        <Text variant={'nav2'}>item2</Text>
+        <Text variant={'nav2'}>item3</Text>
+        <Text variant={'nav2'}>item4</Text>
+        <Text variant={'nav2'}>item5</Text>
+        <Text variant={'nav2'}>item6</Text>
+      </>
+    ),
+  },
+}
---
+++ packages/ui/src/components/flex/flex.tsx
@@ -0,0 +1,77 @@
+import { Box } from '@/components/box'
+import { AtomicProps, LayoutProps, PolymorphicComponentProp, PolymorphicRef } from '@/types'
+import { ElementType, forwardRef } from 'react'
+
+export type FlexElements =
+  | 'div'
+  | 'section'
+  | 'article'
+  | 'aside'
+  | 'header'
+  | 'footer'
+  | 'nav'
+  | 'main'
+  | 'form'
+  | 'fieldset'
+  | 'legend'
+  | 'label'
+  | 'details'
+  | 'summary'
+  | 'figure'
+  | 'figcaption'
+  | 'table'
+  | 'caption'
+  | 'tbody'
+  | 'thead'
+  | 'tr'
+  | 'td'
+  | 'th'
+
+export interface FlexStyleProps extends LayoutProps {
+  /** css gap property */
+  gap?: AtomicProps['gap']
+  /** css flex-direction property */
+  direction?: AtomicProps['flexDirection']
+  /** css align-items property */
+  alignItems?: AtomicProps['alignItems']
+  /** css justify-content property */
+  justifyContent?: AtomicProps['justifyContent']
+  /** css flex property */
+  flex?: AtomicProps['flex']
+  /** css flex-basis property */
+  flexBasis?: AtomicProps['flexBasis']
+  /** css flex-wrap property */
+  flexWrap?: AtomicProps['flexWrap']
+  /** css flex-grow property */
+  flexGrow?: AtomicProps['flexGrow']
+  /** css flex-shrink property */
+  flexShrink?: AtomicProps['flexShrink']
+  /** css background-color property */
+  backgroundColor?: AtomicProps['backgroundColor']
+  /** css color property */
+  color?: AtomicProps['color']
+  /** css border-radius property */
+  borderRadius?: AtomicProps['borderRadius']
+}
+
+export type FlexProps<C extends FlexElements = 'div'> = PolymorphicComponentProp<C, FlexStyleProps>
+
+/** display flex 속성을 기본으로 가지는 레이아웃 컴포넌트 */
+export const Flex = forwardRef(
+  <C extends FlexElements = 'div'>(
+    { children, as, direction, ...rest }: FlexProps<C>,
+    ref: PolymorphicRef<C>,
+  ) => {
+    return (
+      <Box
+        as={(as ?? 'div') as ElementType}
+        display="flex"
+        flexDirection={direction}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Box>
+    )
+  },
+)
