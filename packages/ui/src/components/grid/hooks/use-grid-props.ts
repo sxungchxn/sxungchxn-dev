44196927a9366401118d0ed9b37d1cc69d1ed3e4
Yangseungchan <uvzone@naver.com>
2024-02-12 14:10:47 +0900

    fix: grid props의 inline 누락 사항 반영

--- packages/ui/src/components/grid/hooks/use-grid-props.ts
+++ packages/ui/src/components/grid/hooks/use-grid-props.ts
@@ -4,21 +4,24 @@ import * as styles from '@/components/grid/grid.css'
 import { gridTemplateColumnsVar } from '@/components/grid/grid.css'
 import { BoxProps } from '@/components'
 import { clsx } from 'clsx'
+import { getInlineProps } from '@/styles/utils'
 
 export const useGridProps = <C extends GridElements>({
   style,
   columns,
   gridTemplateColumns,
   className,
-  ...otherGridProps
+  ...others
 }: GridProps<C>) => {
   const gridTemplatecolumnsValue = columns ? `repeat(${columns}, 1fr)` : gridTemplateColumns
+  const { inlineProps, ...otherGridProps } = getInlineProps(others)
 
   return {
     ...otherGridProps,
     display: 'grid',
     style: {
       ...style,
+      ...inlineProps,
       ...assignInlineVars({
         [gridTemplateColumnsVar]: gridTemplatecolumnsValue,
       }),
