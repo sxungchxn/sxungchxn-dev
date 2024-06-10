170ce6f593a8a9ad7eb7f47efaf05d9607edfd1a
sxungchxn <uvzone@naver.com>
2024-06-10 15:54:02 +0900

    chore: ui 수정

--- apps/blog/src/app/blog/[pageId]/components/article-content-renderer/article-content-renderer.css.ts
+++ apps/blog/src/app/blog/[pageId]/components/article-content-renderer/article-content-renderer.css.ts
@@ -3,7 +3,7 @@ import { mediaQuery, vars } from '@sxungchxn/dev-ui'
 
 export const wrapper = style({
   marginTop: '80px',
-  marginBottom: '120px',
+  marginBottom: vars.space['64px'],
   '@media': {
     [mediaQuery.belowPc]: {
       padding: `${vars.space.none} ${vars.space['24px']}`,
--- apps/blog/src/app/blog/[pageId]/components/article-detail-footer-section/article-detail-footer-section.css.ts
+++ apps/blog/src/app/blog/[pageId]/components/article-detail-footer-section/article-detail-footer-section.css.ts
@@ -3,13 +3,15 @@ import { mediaQuery, vars } from '@sxungchxn/dev-ui'
 
 export const wrapper = style({
   width: '100%',
-  gap: vars.space['16px'],
-  marginBottom: vars.space['64px'],
-  padding: `${vars.space.none} ${vars.space['24px']}`,
+  gap: vars.space['20px'],
+  marginBottom: '120px',
   '@media': {
+    [mediaQuery.belowPc]: {
+      padding: `${vars.space.none} ${vars.space['24px']}`,
+    },
     [mediaQuery.aboveMobile]: {
       display: 'grid',
-      gridTemplateColumns: '1fr 1fr',
+      gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
     },
     [mediaQuery.mobile]: {
       display: 'flex',
