c00011eadd1b91d89e8efb63fd7549735fdccd62
sxungchxn <uvzone@naver.com>
2024-07-20 11:47:38 +0900

    feat: anchor 요소 기능 개선

--- apps/blog/src/app/blog/[pageId]/components/article-content-renderer/components/anchor/anchor.tsx
+++ apps/blog/src/app/blog/[pageId]/components/article-content-renderer/components/anchor/anchor.tsx
@@ -1,10 +1,23 @@
 import { Text, type TextProps } from '@sxungchxn/dev-ui'
 import * as styles from './anchor.css'
 import { clsx } from 'clsx'
+import { useEffect, useRef, useState } from 'react'
 
 export interface AnchorProps extends Omit<TextProps<'a'>, 'color' | 'ref' | 'as'> {}
 
+const targetBlankAnchorSelector = 'h2#목차 + ul, h1, h2, h3'
+
 export const Anchor = ({ className, ...props }: AnchorProps) => {
+  const anchorRef = useRef<HTMLAnchorElement>(null)
+  const [isTargetBlank, setIsTargetBlank] = useState(false)
+
+  useEffect(() => {
+    if (anchorRef.current) {
+      const isTargetBlankAnchor = anchorRef.current.closest(targetBlankAnchorSelector) === null
+      setIsTargetBlank(isTargetBlankAnchor)
+    }
+  }, [])
+
   return (
     <Text
       {...props}
@@ -12,6 +25,8 @@ export const Anchor = ({ className, ...props }: AnchorProps) => {
       as="a"
       variant="title3"
       color="textSecondary"
+      ref={anchorRef}
+      {...(isTargetBlank && { target: '_blank' })}
     />
   )
 }
